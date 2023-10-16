50440af802bed03f924f098cbe5f6345cf0c4217
Aryan Gupta <aryangupta02092002@gmail.com>
2023-10-16 13:10:46 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className='navbar-logo'>
+                        TRVL
+                    </Link>
+                </div>
+                
+            </nav>
+        </>
+    </div>
+  )
+}
+
+export default Navbar
