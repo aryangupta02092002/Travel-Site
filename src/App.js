3b60b7b467f77531c30318eb38c2c7f4e1cfffdb
Aryan Gupta <aryangupta02092002@gmail.com>
2023-10-16 23:32:10 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import react from 'react';
+import { Routes ,Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/services' element={<Services/>} />
+          <Route exact path='/products' element={<Products/>} />
+          <Route exact path='/sign-up' element={<SignUp/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
